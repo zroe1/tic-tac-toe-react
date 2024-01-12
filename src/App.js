431c2e7ddf6feeee7322950e47bc217efea2e22a
zephyroe <114773939+zroe1@users.noreply.github.com>
2024-01-12 10:32:21 -0600

    board render at any size

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import { useState } from "react"
 
-function App() {
+function App({n}) {
+  const [board, setBoard] = useState(Array.from({length: n}, () => new Array(n).fill("")));
+
+  const rowSize = {
+    height: `${Math.round(60/n)}vw`
+  }
+  console.log(rowSize.height);
+
+  const boxSize = {
+    height: `${Math.round(60/n)-2}vw`,
+    width: `${Math.round(60/n)-2}vw`
+  }
+
   return (
-    <div>
-      hello world
+    <div className='board-container'>
+      {board.map((row) => {
+        return (
+          <div style={rowSize} className='row'>
+            {row.map((item)=> {
+              return (
+                <div style={boxSize} className='box'>{item}</div>
+              )
+            })}
+          </div>
+        )
+      })}
     </div>
+
   );
 }
 
